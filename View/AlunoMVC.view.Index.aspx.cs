85fc5309ed2c9c5e612df763415756a41be24ac2
andremuriae@yahoo.com.br <andrerorizfernandes>
2023-06-15 19:34:25 -0300

    Concluindo projeto

--- View/AlunoMVC.view.Index.aspx.cs
+++ View/AlunoMVC.view.Index.aspx.cs
@@ -83,6 +83,95 @@ namespace View
             AtualizarDados();
         }
 
+        private void PrepararEdicao(GridViewEditEventArgs pGridViewEditEventArgs)
+        {
+            gvdUsuarios.EditIndex = pGridViewEditEventArgs.NewEditIndex;
+            AtualizarDados();
+        }
+
+        private void CancelarEdicao(GridViewCancelEditEventArgs pGridViewCancelEditEventArgs)
+        {
+            gvdUsuarios.EditIndex = INDICE_INEXISTENTE;
+            AtualizarDados();
+        }
+
+        private void Editar(GridViewUpdateEventArgs pGridViewUpdateEventArgs)
+        {
+            int index = pGridViewUpdateEventArgs.RowIndex;
+
+            Label lblId = gvdUsuarios.Rows[index].FindControl("lblId") as Label;
+            TextBox txtNome = gvdUsuarios.Rows[index].FindControl("txtNome") as TextBox;
+            TextBox txtMatricula = gvdUsuarios.Rows[index].FindControl("txtMatricula") as TextBox;
+            TextBox txtCurso = gvdUsuarios.Rows[index].FindControl("txtCurso") as TextBox;
+
+            //ajustando os valores 
+            if (lblId != null)
+            {
+                txtNome.Text = (txtNome == null) ? "" : txtNome.Text;
+                txtMatricula.Text = (txtMatricula == null) ? "" : txtMatricula.Text;
+                txtCurso.Text = (txtCurso == null) ? "" : txtCurso.Text;
+
+                IAluno a = new Aluno(
+                    Convert.ToInt32(lblId.Text),
+                    txtNome.Text,
+                    txtMatricula.Text,
+                    txtCurso.Text);
+
+                if (a.Nome == string.Empty)
+                {
+                    Mensagem("Informe o nome");
+                }
+                else if (a.Matricula == string.Empty)
+                {
+                    Mensagem("Informe a matrícula");
+                }
+                else if (a.Curso == string.Empty)
+                {
+                    Mensagem("Informe o curso");
+                }
+                else
+                {
+                    if (AlunoController.
+                        NovaInstancia().
+                            Editar(a))
+                    {
+                        Mensagem("Aluno editado com sucesso");
+                    }
+                    else
+                    {
+                        Mensagem("Falha ao editar aluno");
+                    }
+                }
+            }
+
+            gvdUsuarios.EditIndex = INDICE_INEXISTENTE; //indicando que a linha não está mais em edição
+            AtualizarDados();
+        }
+
+        private void Excluir(GridViewDeleteEventArgs pGridViewDeleteEventArgs)
+        {
+            int index = pGridViewDeleteEventArgs.RowIndex;
+            Label lblId = gvdUsuarios.Rows[index].FindControl("lblId") as Label;
+
+            if (lblId != null)
+            {
+
+                IAluno a = new Aluno(Convert.ToInt32(lblId.Text));
+                if( AlunoController.
+                    NovaInstancia().
+                        Excluir(a))
+                {
+                    Mensagem("Aluno excluído com sucesso");
+                }
+                else
+                {
+                    Mensagem("Falha ao excluir aluno");
+                }
+            }
+
+            gvdUsuarios.EditIndex = INDICE_INEXISTENTE;
+            AtualizarDados();
+        }
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -94,22 +183,22 @@ namespace View
 
         protected void gvdUsuarios_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
         {
-
+            CancelarEdicao(e);
         }
 
         protected void gvdUsuarios_RowDeleting(object sender, GridViewDeleteEventArgs e)
         {
-
+            Excluir(e);
         }
 
         protected void gvdUsuarios_RowEditing(object sender, GridViewEditEventArgs e)
         {
-
+            PrepararEdicao(e);
         }
 
         protected void gvdUsuarios_RowUpdating(object sender, GridViewUpdateEventArgs e)
         {
-
+            Editar(e);
         }
 
         protected void btnAdicionarNovo_Click(object sender, EventArgs e)
